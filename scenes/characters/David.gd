8fd3b670cd874778728c373d04f9096a1686c228
naylafn <naylafarah.31@gmail.com>
2026-03-19 21:49:04 +0700

    feat: Add slice animations

--- scenes/characters/David.gd
+++ scenes/characters/David.gd
@@ -5,8 +5,12 @@ extends CharacterBody2D
 @onready var anim = $AnimatedSprite2D
 
 var last_direction: Vector2 = Vector2.DOWN  # default menghadap bawah
+var is_attacking: bool = false
 
 func _physics_process(delta):
+	if is_attacking:
+		return
+		
 	var direction = Vector2.ZERO
 
 	# Input movement
@@ -23,7 +27,19 @@ func _physics_process(delta):
 
 	handle_animation(direction)
 
+func _input(event):
+	if event.is_action_pressed("mouse_left"):
+		start_slice()
 
+func start_slice():
+	if is_attacking:
+		return
+	
+	is_attacking = true
+	velocity = Vector2.ZERO
+
+	play_slice_animation()
+	
 func handle_animation(direction: Vector2):
 	if direction == Vector2.ZERO:
 		play_idle_animation()
@@ -40,7 +56,6 @@ func handle_animation(direction: Vector2):
 		else:
 			anim.play("walk_up")
 
-
 func play_idle_animation():
 	if abs(last_direction.x) > abs(last_direction.y):
 		# Horizontal idle
@@ -52,3 +67,19 @@ func play_idle_animation():
 			anim.play("idle_down")
 		else:
 			anim.play("idle_up")
+
+func play_slice_animation():
+	if abs(last_direction.x) > abs(last_direction.y):
+		anim.play("slice_side")
+		anim.flip_h = last_direction.x < 0
+	else:
+		if last_direction.y > 0:
+			anim.play("slice_down")
+		else:
+			anim.play("slice_up")
+
+
+# Signals
+func _on_animated_sprite_2d_animation_finished():
+	if anim.animation.begins_with("slice"):
+		is_attacking = false
