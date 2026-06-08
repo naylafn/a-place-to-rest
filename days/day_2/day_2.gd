4a1b9d148b305ab14e23c520ea544d645bfd89b5
naylafn <naylafarah.31@gmail.com>
2026-06-08 13:37:10 +0700

    feat: add new tasks for day 2

--- days/day_2/day_2.gd
+++ days/day_2/day_2.gd
@@ -4,6 +4,10 @@ extends Node2D
 @onready var player = $David
 
 var bridge_investigated := false
+var total_crops := 0
+var watered_crops := 0
+var total_grass := 0
+var cut_grass := 0
 var total_wood := 0
 var cut_wood := 0
 
@@ -23,8 +27,20 @@ func _ready():
 		var spawn_point = get_node(SceneTransition.spawn_point_name)
 		player.global_position = spawn_point.global_position
 		SceneTransition.spawn_point_name = ""
-		
-	show_investigate_objective()
+
+	var crops = get_tree().get_nodes_in_group("crops")
+	total_crops = crops.size()
+
+	for crop in crops:
+		crop.watered.connect(_on_crop_watered)
+
+	var grass_map = get_node_or_null("GrassMap")
+	if grass_map:
+		var grasses = grass_map.get_children()
+		total_grass = grasses.size()
+
+		for grass in grasses:
+			grass.cut.connect(_on_grass_cut)
 
 	var woods = get_tree().get_nodes_in_group("source")
 	total_wood = woods.size()
@@ -35,26 +51,34 @@ func _ready():
 	var broken_bridge = $BrokenBridge
 	broken_bridge.bridge_investigated.connect(_on_bridge_investigated)
 
-func show_investigate_objective():
-	objective_label.text = "Investigate the footprints"
+	update_objective_ui()
+
+func update_objective_ui():
+	if not bridge_investigated:
+		objective_label.text = "Investigate the footprints"
+		return
+
+	objective_label.text = "Water crops: %d / %d\nCut grass: %d / %d\nCollect wood: %d / %d" % [watered_crops, total_crops, cut_grass, total_grass, cut_wood, total_wood]
+
+	if watered_crops >= total_crops and cut_grass >= total_grass and cut_wood >= total_wood:
+		GameState.day_2_wood_done = true
+		objective_label.text = "Return to the broken bridge."
 
 func _on_bridge_investigated():
 	bridge_investigated = true
-	update_wood_objective()
+	update_objective_ui()
+
+func _on_crop_watered():
+	watered_crops += 1
+	update_objective_ui()
+
+func _on_grass_cut():
+	cut_grass += 1
+	update_objective_ui()
 
 func _on_wood_cut():
-	if not bridge_investigated:
-		return
-
 	cut_wood += 1
-	update_wood_objective()
-
-func update_wood_objective():
-	objective_label.text = "Collect wood: %d / %d" % [cut_wood, total_wood]
-
-	if cut_wood >= total_wood:
-		GameState.day_2_wood_done = true
-		objective_label.text = "Return to the broken bridge."
+	update_objective_ui()
 
 func _on_dialogic_timeline_ended():
 	player.set_can_move(true)
